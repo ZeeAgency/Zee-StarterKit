992daef6b73b2afab3abdfee86777bb6c01282e9
jcabanes@aaz.fr <Julien@iMac-Ju.local>
2011-01-19 18:46:20 +0100

    Added window.console alternative

---
+++ js/zee.console.js
@@ -0,0 +1,17 @@
+if(!window.console) {
+	window.console = {
+		addError: function(args, level) {
+			args = Array.prototype.slice.call(args);
+			$('#zee-console').append('<div class="zee-console-'+level+'">'+args.join(', ')+'</div>');
+		},
+		log: function() {
+			this.addError(arguments, 'log');
+		},
+		error: function() {
+			this.addError(arguments, 'log');
+		},
+		warn: function() {
+			this.addError(arguments, 'log');
+		}
+	};
+}
