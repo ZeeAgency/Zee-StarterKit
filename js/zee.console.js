d00c9a94daf70a66bf5b6755f0805400a19421d5
Zee Agency <jc@aaz.fr>
2011-01-31 02:09:24 +0100

    Added YepNope and Selectivizr

--- js/zee.console.js
+++ js/zee.console.js
@@ -1,17 +1,15 @@
-if(!window.console) {
-	window.console = {
-		addError: function(args, level) {
-			args = Array.prototype.slice.call(args);
-			$('#zee-console').append('<div class="zee-console-'+level+'">'+args.join(', ')+'</div>');
-		},
-		log: function() {
-			this.addError(arguments, 'log');
-		},
-		error: function() {
-			this.addError(arguments, 'log');
-		},
-		warn: function() {
-			this.addError(arguments, 'log');
-		}
-	};
-}
+window.console = {
+	addError: function(args, level) {
+		args = Array.prototype.slice.call(args);
+		$('#zee-console').append('<div class="zee-console-'+level+'">'+args.join(', ')+'</div>');
+	},
+	log: function() {
+		this.addError(arguments, 'log');
+	},
+	error: function() {
+		this.addError(arguments, 'log');
+	},
+	warn: function() {
+		this.addError(arguments, 'log');
+	}
+};
